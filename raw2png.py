a927381d099bc02394af7c0724ec554473898c32
Marc-Alexandre Espiaut <76531574+malespiaut@users.noreply.github.com>
2024-04-07 15:56:55 +0200

    Using BytesIO instead of directly reading the file on disk.

--- raw2png.py
+++ raw2png.py
@@ -17,6 +17,7 @@ Version: 240326
 # - http://tex.imm.uran.ru/alchemy.pdf
 
 
+import io
 import logging
 import os
 import sys
@@ -36,32 +37,36 @@ def main():
     """Everything happens here."""
     logging.basicConfig(level=logging.INFO)
 
+    file = None
     with open(sys.argv[1], "rb") as f:
-        logging.info(f"Reading {sys.argv[1]}")
+        logging.info(f"Storing {sys.argv[1]} in RAM.")
+        file = f.read()
+        f.close()
 
+    with io.BytesIO(file) as r:
         # Checking is this is a real “mhwanh” file
-        if str(f.read(6), "ascii") == "mhwanh":
+        if str(r.read(6), "ascii") == "mhwanh":
             logging.info("Signature is correct")
 
             # Jumping directly to the image dimensions.
-            f.seek(0x8)
+            r.seek(0x8)
 
             # Reading header
-            image_width: int = int.from_bytes(f.read(2), byteorder="big")
-            image_height: int = int.from_bytes(f.read(2), byteorder="big")
+            image_width: int = int.from_bytes(r.read(2), byteorder="big")
+            image_height: int = int.from_bytes(r.read(2), byteorder="big")
             logging.info(f"Image size: {image_width}x{image_height}")
 
-            palette_colors: int = int.from_bytes(f.read(2), byteorder="big")
+            palette_colors: int = int.from_bytes(r.read(2), byteorder="big")
 
             # Jumping directly to the image data.
-            f.seek(0x20)
+            r.seek(0x20)
 
             if palette_colors != 0:
                 logging.info("This image have a color palette!")
 
                 # Writing palette to a file, and saving it in memory
                 with open(filename_gen(sys.argv[1], "pal"), "wb") as output_palette:
-                    palette: bytes = f.read(768)
+                    palette: bytes = r.read(768)
                     output_palette.write(palette)
                     output_palette.close()
 
@@ -69,7 +74,7 @@ def main():
                     output_image: Image.Image = Image.frombytes(
                         "P",
                         (image_width, image_height),
-                        f.read(image_width * image_height),
+                        r.read(image_width * image_height),
                     )
                     output_image.putpalette(palette)
                     output_image.save(filename_gen(sys.argv[1], "png"))
@@ -79,7 +84,7 @@ def main():
 
                 # Writing the RGB image
                 Image.fromarray(
-                    np.frombuffer(f.read(), dtype=np.uint8).reshape(
+                    np.frombuffer(r.read(), dtype=np.uint8).reshape(
                         (image_height, image_width, 3)
                     )
                 ).save(filename_gen(sys.argv[1], "png"))
